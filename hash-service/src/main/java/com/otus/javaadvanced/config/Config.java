619feaca935c25846a59da3b1196f123c9c2b3ee
al-xandr1 <al-xandr1@yandex.ru>
2024-02-12 22:01:31 +0400

    Second variant of Hash service with bugs (First open lesson)

--- hash-service/src/main/java/com/otus/javaadvanced/config/Config.java
+++ hash-service/src/main/java/com/otus/javaadvanced/config/Config.java
@@ -1,15 +1,21 @@
 package com.otus.javaadvanced.config;
 
+import com.otus.javaadvanced.services.EncodeService;
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Metrics {
+public class Config {
 
     @Bean
     public TimedAspect timedAspect(MeterRegistry registry) {
         return new TimedAspect(registry);
+    }
+
+    @Bean
+    public EncodeService encodeService() {
+        return new EncodeService();
     }
 }
---
+++ hash-service/src/main/java/com/otus/javaadvanced/services/Encoder.java
@@ -0,0 +1,6 @@
+package com.otus.javaadvanced.services;
+
+public interface Encoder {
+
+    byte[] encode(byte[] src) throws Exception;
+}
