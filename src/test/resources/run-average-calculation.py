67f12780641fb75969a3d36dc2c745fe0d9e3cd0
al-xandr1 <al-xandr1@yandex.ru>
2024-02-12 21:17:45 +0400

    Change format

--- src/test/resources/run-average-calculation.py
+++ src/test/resources/run-average-calculation.py
@@ -2,14 +2,16 @@ import requests
 import time
 
 METRICS_URI = "http://localhost:8080/actuator/metrics/{}"
+METRIC = "hash.time"
 
 if __name__ == '__main__':
     while True:
-        response = requests.get(METRICS_URI.format("hash.time"))
+        response = requests.get(METRICS_URI.format(METRIC))
         json = response.json()
         # print(f'Response json={json}')
         count = json["measurements"][0]["value"]  # 0 -> COUNT
         total = json["measurements"][1]["value"]  # 1 -> TOTAL_TIME
 
-        print(f'avg: {total / count}         = {total} / {count} = TOTAL_TIME / COUNT ')  # format(total, '.12g')
+        print(
+            f'{METRIC} avg (sec): {total / count}         = {total} / {count} = TOTAL_TIME / COUNT ')  # format(total, '.12g')
         time.sleep(1)
