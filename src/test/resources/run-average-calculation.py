519cb891954c13eaea673021b42528ef3c8a2d42
al-xandr1 <al-xandr1@yandex.ru>
2024-02-12 20:50:26 +0400

    Change format

--- src/test/resources/run-average-calculation.py
+++ src/test/resources/run-average-calculation.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
         count = json["measurements"][0]["value"]  # 0 -> COUNT
         total = json["measurements"][1]["value"]  # 1 -> TOTAL_TIME
 
-        print(f'average: TOTAL_TIME / COUNT = {total} / {count} = {total / count}')  # format(total, '.12g')
+        print(f'avg: {total / count}         = {total} / {count} = TOTAL_TIME / COUNT ')  # format(total, '.12g')
         time.sleep(1)
