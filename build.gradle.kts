5d2cd2961b062329a70b79de83e46cf76b5afac8
al-xandr1 <al-xandr1@yandex.ru>
2024-02-12 15:26:27 +0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.0.3"
+}
+apply(plugin = "io.spring.dependency-management")
+
+group = "com.otus.java.advanced"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "java-advanced-course"
+
