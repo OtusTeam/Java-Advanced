366af5677a076ccd118d59fef20601203076b9bc
al-xandr1 <al-xandr1@yandex.ru>
2024-02-12 22:01:31 +0400

    Prepare Hash Service with bugs & fix patches (Open lesson 1)

---
+++ hash-service/src/main/java/com/otus/javaadvanced/services/EncodeService.java
@@ -0,0 +1,36 @@
+package com.otus.javaadvanced.services;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Base64;
+
+@RequiredArgsConstructor
+public class EncodeService {
+
+    private Encoder encoder;
+
+    private static final int STACK_DEPTH = 100;
+
+    public byte[] encode(byte[] hash) {
+        byte[] safe = Arrays.copyOf(hash, hash.length);
+        return encodeInner(safe, 0);
+    }
+
+    private byte[] encodeInner(byte[] hash, int level) {
+        if (level < STACK_DEPTH) {
+            // imitation of long stacktrace
+            return encodeInner(hash, ++level);
+        }
+
+        byte[] result;
+        try {
+            result = encoder.encode(hash);
+
+        } catch (Exception e) {
+            result = Base64.getEncoder().encode(hash);
+        }
+
+        return result;
+    }
+}
--- hash-service/src/main/java/com/otus/javaadvanced/services/HashService.java
+++ hash-service/src/main/java/com/otus/javaadvanced/services/HashService.java
@@ -1,11 +1,47 @@
 package com.otus.javaadvanced.services;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 @Service
+@AllArgsConstructor
 public class HashService {
 
+    private static final int MAX_HASH_COUNT = 200;
+
+    private final EncodeService encodeService;
+
     public String hash(String input) {
-        return String.valueOf(input.hashCode());
+        String result = input;
+        for (int i = 0; i < MAX_HASH_COUNT; i++) {
+            result = hashIteration(result);
+        }
+
+        return result;
+    }
+
+    // https://www.baeldung.com/java-password-hashing
+    public String hashIteration(String input) {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        md.update(salt);
+
+        byte[] hashedPassword = md.digest(input.getBytes(StandardCharsets.UTF_8));
+
+        byte[] encode = encodeService.encode(hashedPassword);
+        return new String(encode);
     }
 }
