60de1cc933c275e35f71d7aed86980c866518caa
al-xandr1 <al-xandr1@yandex.ru>
2024-03-06 18:28:31 +0400

    Add exception count logic with bugs & fix patches (Open lesson 2)

---
+++ hash-service/src/main/java/com/otus/javaadvanced/services/ExceptionUtils.java
@@ -0,0 +1,43 @@
+package com.otus.javaadvanced.services;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Slf4j
+public final class ExceptionUtils {
+
+    private static long exceptionCount = 0;
+
+    private static Lock lock = new ReentrantLock();
+
+    public static long countException(Exception e) {
+        while (!lock.tryLock()) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(300);
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+
+        try {
+            return ++exceptionCount;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void acceptCount(long count) {
+        long index = 0;
+        for (int i = 0; i < 1_000_000; i++) {
+            // it's fake logic :)
+            index = count % 100;
+        }
+
+        if (index == 0) {
+            log.info("Current exception count: {}", count);
+        }
+    }
+}
