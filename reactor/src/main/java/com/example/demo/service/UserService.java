3dce171a579415add7d43d03a753ab83b98d43a9
maksimmakarenko <m.makarenko.dev@gmail.com>
2025-05-05 23:22:47 -0500

    add MacOS support

--- reactor/src/main/java/com/example/demo/service/UserService.java
+++ reactor/src/main/java/com/example/demo/service/UserService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -24,6 +26,16 @@ public class UserService {
 
     public Flux<String> getUsersEmails() {
         return repository.getAllEmails();
+    }
+
+    public List<String> getUsersEmailsBlocking() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        return List.of("alex@mail.ru", "anastasia@mail.ru", "gleb@mail.ru");
     }
 
     public Mono<User> update(Integer id, String name, String email) {
