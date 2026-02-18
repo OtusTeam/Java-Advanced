6f12977995eb892b74768a214c7f89e6424457b1
xandr <al-xandr1@yandex.ru>
2026-02-18 19:31:50 +0400

    Fix cds bash scripts

--- hash-service/src/main/java/com/otus/javaadvanced/HashApplication.java
+++ hash-service/src/main/java/com/otus/javaadvanced/HashApplication.java
@@ -1,23 +1,27 @@
 package com.otus.javaadvanced;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 @SpringBootApplication
+@Slf4j
 @EnableAspectJAutoProxy
-public class HashApplication /*implements ApplicationListener<ContextRefreshedEvent> */ {
+public class HashApplication implements ApplicationListener<ApplicationStartedEvent> {
 
     public static void main(String[] args) {
         SpringApplication.run(HashApplication.class, args);
     }
 
     //region lesson CDS & ProjectLeyden
-/*
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
+    public void onApplicationEvent(ApplicationStartedEvent event) {
+        log.info("Application started. Exiting...");
         Runtime.getRuntime().halt(-777);
     }
-*/
     //endregion
 }
